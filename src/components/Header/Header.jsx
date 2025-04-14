ff382f63d32a22eeccf7ddfcb119722b24989a03
Tuigun <akysbekovtuigun@gmail.com>
2025-04-14 09:12:35 +0600

    tykee-A13

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,74 @@
 import React from 'react'
 import "./header.css"
+import logo from "../../assets/logo.svg"
+import lin from "../../assets/lin.svg"
+import search from "../../assets/search.svg"
+import insta from "../../assets/insta.svg"
+import teleg from "../../assets/teleg.svg"
+import whats from "../../assets/whats.svg"
+import akk from "../../assets/akk.svg"
+import like from "../../assets/like.svg"
+import backet from "../../assets/backet.svg"
+import {Link} from "react-router-dom"
 function Header() {
   return (
-    <div>
-      Header
+    <>
+    <div className='header container'>
+      <div className="header-left">
+        <img src={logo} alt="" />
+        <div className="header-text">
+        <h3>КРАСНАЯ ТУФЕЛЬКА</h3>
+        <p>магазин женской обуви</p>
+        </div>
+        <div className="katalo">
+          <img src={lin} alt="" />
+          <p>Каталог обуви</p>
+        </div>
+      </div>
+      <div className="header-cent">
+        <input type="text" placeholder='Введите артикул или название товара...'/>
+        <div className="search">
+          <img src={search} alt="" />
+        </div>
+      </div>
+      <div className="header-right">
+        <div className="net">
+            <img src={insta} alt="" />
+            <div className="teleg">
+              <img src={teleg} alt="" />
+            </div>
+            <div className="whats">
+              <img src={whats} alt="" />
+            </div>
+        </div>
+        <div className="net2">
+          <div className="conect">
+            <img src={akk} alt="" />
+          </div>
+          <div className="conect">
+            <img src={like} alt="" />
+          </div>
+          <div className="conect">
+            <img src={backet} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
+      {/* -----------------------------header-down */}
+      <hr className='container' />
+    <div className="down container">
+     <ul>
+        <Link>Новинки</Link>
+        <Link>Распродажа</Link>
+        <Link>Оплата и доставка</Link>
+        <Link>Обмен и возврат</Link>
+        <Link>Магазины</Link>
+        <Link>О нас</Link>
+        <Link>Полезное</Link>
+        <Link>Контакты</Link>
+      </ul>
+    </div>
+    </>
   )
 }
 
---
+++ src/components/New-collections/Collections.jsx
@@ -0,0 +1,230 @@
+import React from 'react'
+import "./Collections.css"
+import like from "../New-collections/svg/like.svg"
+import right from "../New-collections/svg/right.svg"
+import kros1 from "../New-collections/img/kros1.png"
+import kros2 from "../New-collections/img/kros2.png"
+import kros3 from "../New-collections/img/kros3.png"
+import kros4 from "../New-collections/img/kros4.png"
+import kros5 from "../New-collections/img/kros5.png"
+import kros6 from "../New-collections/img/kros6.png"
+import kros7 from "../New-collections/img/kros7.png"
+import kros8 from "../New-collections/img/kros8.png"
+import kros9 from "../New-collections/img/kros9.png"
+import women from "../New-collections/img/women.png"
+function Collections() {
+  return (
+    <>
+      <div className="Collect">
+        <h2>Новая коллекция «Весна 2022» </h2>
+        <ul>
+            <li>Вся обувь</li>
+            <li>Батильоны</li>
+            <li>Ботинки</li>
+            <li>Ботфорты</li>
+            <li>Кеды</li>
+            <li>Кросовки</li>
+            <li>Дерби</li>
+            <li>Броги</li>
+        </ul>
+      </div>
+
+        <div className="krose">
+            {/* 1 */}
+            <div className="new">
+                <img src={kros1} alt="" />
+            <div className="top">
+                <div className="left">
+                    <p>Артикул: 515314</p>
+                    <p>Батильоны</p>
+                </div>
+                <div className="right">
+                        <img src={like} alt="" />
+                </div>
+            </div>
+            <div className="pot">
+                <h3>Ботильоны Wilmar черные с цепочкой</h3>
+                    <div className="price">
+                        <p>5 100</p>
+                        <span>Новинка</span>
+                    </div>
+            </div>
+            </div>
+            {/* 2 */}
+            <div className="new">
+                <img src={kros2} alt="" />
+            <div className="top">
+                <div className="left">
+                    <p>Артикул: 515314</p>
+                    <p>Батильоны</p>
+                </div>
+                <div className="right">
+                        <img src={like} alt="" />
+                </div>
+            </div>
+            <div className="pot">
+                <h3>Ботильоны Wilmar черные с цепочкой</h3>
+                    <div className="price">
+                        <p>5 100</p>
+                        <span>Новинка</span>
+                    </div>
+            </div>
+            </div>
+            {/* 3 */}
+            <div className="new">
+                <img src={kros3} alt="" />
+            <div className="top">
+                <div className="left">
+                    <p>Артикул: 515314</p>
+                    <p>Батильоны</p>
+                </div>
+                <div className="right">
+                        <img src={like} alt="" />
+                </div>
+            </div>
+            <div className="pot">
+                <h3>Ботильоны Wilmar черные с цепочкой</h3>
+                    <div className="price">
+                        <p>5 100</p>
+                        <span>Новинка</span>
+                    </div>
+            </div>
+            </div>
+            {/* 4 */}
+            <div className="new">
+                <img src={kros4} alt="" />
+            <div className="top">
+                <div className="left">
+                    <p>Артикул: 515314</p>
+                    <p>Батильоны</p>
+                </div>
+                <div className="right">
+                        <img src={like} alt="" />
+                </div>
+            </div>
+            <div className="pot">
+                <h3>Ботильоны Wilmar черные с цепочкой</h3>
+                    <div className="price">
+                        <p>5 100</p>
+                        <span>Новинка</span>
+                    </div>
+            </div>
+            </div>
+                {/* 9 */}
+                <div className="new">
+                <img src={kros9} alt="" />
+            <div className="top">
+                <div className="left">
+                    <p>Артикул: 515314</p>
+                    <p>Батильоны</p>
+                </div>
+                <div className="right">
+                        <img src={like} alt="" />
+                </div>
+            </div>
+            <div className="pot">
+                <h3>Ботильоны Wilmar черные с цепочкой</h3>
+                    <div className="price">
+                        <p>5 100</p>
+                        <span>Новинка</span>
+                    </div>
+            </div>
+            </div>
+            {/* 5 */}
+            <div className="new">
+                <img src={kros5} alt="" />
+            <div className="top">
+                <div className="left">
+                    <p>Артикул: 515314</p>
+                    <p>Батильоны</p>
+                </div>
+                <div className="right">
+                        <img src={like} alt="" />
+                </div>
+            </div>
+            <div className="pot">
+                <h3>Ботильоны Wilmar черные с цепочкой</h3>
+                    <div className="price">
+                        <p>5 100</p>
+                        <span>Новинка</span>
+                    </div>
+            </div>
+            </div>
+            {/* 6 */}
+            <div className="new">
+                <img src={kros6} alt="" />
+            <div className="top">
+                <div className="left">
+                    <p>Артикул: 515314</p>
+                    <p>Батильоны</p>
+                </div>
+                <div className="right">
+                        <img src={like} alt="" />
+                </div>
+            </div>
+            <div className="pot">
+                <h3>Ботильоны Wilmar черные с цепочкой</h3>
+                    <div className="price">
+                        <p>5 100</p>
+                        <span>Новинка</span>
+                    </div>
+            </div>
+            </div>
+            {/* 7 */}
+            <div className="new">
+                <img src={kros7} alt="" />
+            <div className="top">
+                <div className="left">
+                    <p>Артикул: 515314</p>
+                    <p>Батильоны</p>
+                </div>
+                <div className="right">
+                        <img src={like} alt="" />
+                </div>
+            </div>
+            <div className="pot">
+                <h3>Ботильоны Wilmar черные с цепочкой</h3>
+                    <div className="price">
+                        <p>5 100</p>
+                        <span>Новинка</span>
+                    </div>
+            </div>
+            </div>
+
+            {/* 8 */}
+            <div className="new">
+                <img src={kros8} alt="" />
+            <div className="top">
+                <div className="left">
+                    <p>Артикул: 515314</p>
+                    <p>Батильоны</p>
+                </div>
+                <div className="right">
+                        <img src={like} alt="" />
+                </div>
+            </div>
+            <div className="pot">
+                <h3>Ботильоны Wilmar черные с цепочкой</h3>
+                    <div className="price">
+                        <p>5 100</p>
+                        <span>Новинка</span>
+                    </div>
+            </div>
+            </div>
+
+            {/* 10 */}
+            
+               <div className="lol">
+               <h3>Перейти в раздел </h3>
+               <img className='art1' src={women} alt="" />
+               <h2>БАТИЛЬОНЫ</h2>
+                <img className='art2' src={right} alt="" />
+               </div>
+
+
+        </div>
+    </>
+  )
+}
+
+export default Collections
