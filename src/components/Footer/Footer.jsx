7592e67d9efdd9816ae86cc342be6dd67ee01cd2
Tuigun <akysbekovtuigun@gmail.com>
2025-04-18 09:14:34 +0600

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import "./footer.css"
+import logo1 from "../../assets/logo.svg"
 function Footer() {
   return (
-    <div className='container'>
-      Footer
+    <div className="container">
+      <div className="footer">
+        <div className="logo1">
+          <img src={logo1} alt="" />
+          <div className="title">
+            <h3>КРАСНАЯ ТУФЕЛЬКА</h3>
+            <p>магазин женской обуви</p>
+          </div>
+        </div>
+          <hr />
+          <div className="info">
+              <h4>Общая информация</h4>
+            <div>
+              <p>Главная</p>
+              <p>Новинки</p>
+              <p>Распродажа</p>
+              <p>Оплата и доставка</p>
+              <p>Обмен и возврат</p>
+              <p>Магазины</p>
+              <p>О нас</p>
+              <p>Полезное</p>
+              <p>Контакты</p>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../components/Banner/Banner'
 import Collections from '../components/New-collections/Collections'
 import Catolog from '../components/Catolog/Catolog'
 import Frameii from '../components/Frame.ii/Frame.ii'
+import Sub from '../components/Sub/Sub'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Collections/>
       <Catolog/>
       <Frameii/>
+      <Sub/>
     </div>
   )
 }
